80541b2559b1138d7e3ad55782b22487f617c05c
deepa3006 <deepu.palabhavi@gmail.com>
2023-02-10 15:28:27 +0100

    Updating libraries

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def twitter_scrapper(request):
         except Exception as ex:
             raise ex
         tweets_df = pd.concat([tweets_df,pd.DataFrame.from_records([{'user_name': tweet.user.name, 
-                                                'user_location': tweet.user.location,\
+                                                'user_location': tweet.user.location,
                                                 'user_description': tweet.user.description,
                                                 #'user_verified': tweet.user.verified,
                                                 #'created_date': tweet.created_at,
